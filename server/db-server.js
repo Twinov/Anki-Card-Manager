8ee073bf49072eeb02ae490c2733d6887d9c18b2
Twinov <hoeltgemarc@gmail.com>
2021-07-25 23:25:13 -0400

    sentence server complete

--- server/db-server.js
+++ server/db-server.js
@@ -1,4 +1,4 @@
-const http = require('http')
+const express = require('express')
 const sqlite3 = require('sqlite3').verbose()
 
 let db = new sqlite3.Database('./chinese_sentences.sqlite', sqlite3.OPEN_READONLY, (err) => {
@@ -8,21 +8,32 @@ let db = new sqlite3.Database('./chinese_sentences.sqlite', sqlite3.OPEN_READONL
   console.log('connected to example sentences database')
 })
 
-const requestListener = function (req, res) {
-  res.writeHead(200)
-  res.end('Hello, World!')
-}
+const app = express()
+const port = 3003
 
-let sql = `SELECT traditional FROM examples limit 5`
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-db.all(sql, [], (err, rows) => {
-  if (err) {
-    throw err
-  }
-  rows.forEach((row) => {
-    console.log(row)
+app.post('/api/sentence_for_character', (req, res) => {
+  const sql = `SELECT * FROM examples WHERE traditional LIKE '%${req.body.character}%' LIMIT 5`
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.message })
+      return console.err(err.message)
+    }
+    const traditional = []
+    const simplified = []
+    console.log(`sentences for ${req.body.character}:`)
+    rows.forEach((row) => {
+      console.log(row.traditional, row.simplified)
+      traditional.push(row.traditional)
+      simplified.push(row.simplified)
+    })
+    console.log('')
+    res.json({ traditional: traditional, simplified: simplified })
   })
 })
 
-const server = http.createServer(requestListener)
-server.listen(8080)
+app.listen(port, () => {
+  console.log(`Sentence database API listening on localhost port ${port}`)
+})
